fe52100af3f884bf11117784517cc7419d45f487
shoutingsharma <akashkofficial54@gmail.com>
2023-03-31 11:19:05 +0530

    making views part of of application

--- index.js
+++ index.js
@@ -3,7 +3,10 @@ const app = express();
 const port = 8000;
 
 //use express router
-app.use('/',require('./routes/index'))
+app.use('/',require('./routes/index'));
+//set up view engine
+ app.set('view engine','ejs');
+ app.set('views','./views');
 
 
 
--- routes/index.js
+++ routes/index.js
@@ -1,19 +1,16 @@
 const express = require("express");
 
 const router = express.Router();
- const homeController = require('../controllers/home_controller');
- 
+const homeController = require("../controllers/home_controller");
 
-console.log('router loaded');
+console.log("router loaded");
 
-router.get('/',homeController.home);
+router.get("/", homeController.home);
 
-router.use('/users',require('./users'));
+router.use("/users", require("./users"));
 
 //for any further routes,access from here
 //router.use('/routerName',require('./routerfile))
-router.use('/posts',require('./posts'));
-
-
+router.use("/posts", require("./posts"));
 
 module.exports = router;
